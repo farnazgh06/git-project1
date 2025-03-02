195539510f937de2a76b2278da3972987ee045b8
farnazgh06 <whoisfarnaz@gmail.com>
2025-03-02 22:15:25 +0330

    Create thankYouPage.java

---
+++ src/thankYouPage.java
@@ -0,0 +1,31 @@
+package packages;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class thankYouPage {
+
+    public void create(JFrame frame) {
+
+        // Create and configure the panel
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 600, 600);
+
+        // Create thank you label
+        JLabel thankYouLabel = new JLabel("Thanks for your participation!");
+        thankYouLabel.setFont(new Font("Arial",Font.BOLD, 13));
+        thankYouLabel.setBounds(10, 10, 200, 20);
+
+        // Add components to the panel
+        panel.add(thankYouLabel);
+
+        // Add panel to the frame
+        frame.add(panel);
+
+
+    }
+
+
+}
+
